05253f96f569a070da4233bb8bbc3112dbc2c294
Yuri Camilo <123594124+YurissCG@users.noreply.github.com>
2024-02-23 12:39:54 -0300

    Add files via upload
    Desafio – membros estatísticos

---
+++ DesafioMembrosEstatisticos/src/CalcularArea.java
@@ -0,0 +1,11 @@
+public class CalcularArea {
+
+    static double areaQuadrado(double lado){
+        return lado * lado;
+    }
+
+    static double areaCirculo(double raio){
+        double pi = 3.14159265358979323846;
+        return (raio * raio) * pi;
+    }
+}
---
+++ DesafioMembrosEstatisticos/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("A area do Quadrado é = "+CalcularArea.areaQuadrado(2));
+        System.out.println("A area do Circulo é = "+CalcularArea.areaCirculo(10));
+    }
+}
